69368ce5cbbe68369ec25411de18e5ec96331e2a
siandav <sian.davies@ironhack.com>
2020-10-29 08:57:05 +0100

    typo in query fixed

--- Other/sql day 4.sql
+++ Other/sql day 4.sql
@@ -80,14 +80,24 @@ having Avg_Balance > 100000
 order by duration, status;
 
 
+
+
+
+#window fx
+
 select loan_id, account_id, amount, payments, duration, amount-payments as "Balance",
 avg(amount-payments) over (partition by duration) as Avg_Balance
 from bank.loan
 where amount > 100000
 order by duration, balance desc;
+
+
+
 -- You can also have ORDER BY clause followed by partition by as shown below:
+
+
 select loan_id, account_id, amount, payments, duration, amount-payments as "Balance",
-avg(amount-payments) over (partition by duration order by duration asc, amounts desc) as Avg_Balance
+avg(amount-payments) over (partition by duration order by duration asc, amount desc) as Avg_Balance
 from bank.loan
 where amount > 100000;
 
